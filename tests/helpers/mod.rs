75bcb58ee3e72b71958a471a6d49362135d0e591
turkeyzawa <18262943+turkeyzawa@users.noreply.github.com>
2019-11-20 23:24:17 +0900

    Spike

--- tests/helpers/mod.rs
+++ tests/helpers/mod.rs
@@ -1,9 +1,14 @@
+mod middleware;
+
 extern crate actix;
+extern crate actix_service;
 extern crate actix_web;
 extern crate futures;
 
+use actix_service::{Service, Transform};
 use actix_web::{dev, web, App, HttpRequest, HttpResponse, HttpServer};
 use futures::prelude::*;
+use middleware::request_json_writer::Logging;
 use std::sync::mpsc;
 
 pub struct TestServer {
@@ -21,11 +26,12 @@ impl TestServer {
             let sys = actix::System::new("http-server");
 
             let host = std::env::var("API_BASE_URL").unwrap();
-            let addr = HttpServer::new(|| App::new().route("/", web::get().to(index)))
-                .bind(host.replace("https://", "").replace("http://", ""))
-                .unwrap()
-                .shutdown_timeout(5)
-                .start();
+            let addr =
+                HttpServer::new(|| App::new().wrap(Logging).route("/", web::get().to(index)))
+                    .bind(host.replace("https://", "").replace("http://", ""))
+                    .unwrap()
+                    .shutdown_timeout(5)
+                    .start();
 
             let _ = tx.send(addr);
             let _ = sys.run();
@@ -33,6 +39,7 @@ impl TestServer {
         let addr = rx.recv().unwrap();
         Self { addr }
     }
+
     pub fn stop(&self) -> () {
         let _ = self
             .addr
